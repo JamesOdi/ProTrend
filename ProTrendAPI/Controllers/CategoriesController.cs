bcfe2314a0dc5f04df0ec374a5aa78b3a31053b5
Ayo-mi <harabprince@gmail.com>
2022-10-24 10:56:43 +0100

    Removed mobile end from Categories controller

--- ProTrendAPI/Controllers/CategoriesController.cs
+++ ProTrendAPI/Controllers/CategoriesController.cs
@@ -6,7 +6,7 @@ namespace ProTrendAPI.Controllers
 {
     [Route("api/category")]
     [ApiController]
-    [CookieAuthenticationFilter]
+    [ProTrndAuthorizationFilter]
     public class CategoriesController : BaseController
     {
         public CategoriesController(IServiceProvider serviceProvider) : base(serviceProvider) { }
@@ -28,27 +28,6 @@ namespace ProTrendAPI.Controllers
 
         [HttpGet("get/{name}")]
         public async Task<ActionResult<ActionResponse>> GetCategories(string name)
-        {
-            return Ok(new ActionResponse { Successful = true, StatusCode = 200, Message = ActionResponseMessage.Ok, Data = await _categoriesService.GetCategoriesAsync(name) });
-        }
-
-        [HttpGet("mobile/get/{name}/1")]
-        public async Task<ActionResult<ActionResponse>> GetMobileCategory(string name)
-        {
-            var category = await _categoriesService.GetSingleCategory(name);
-            if (category == null)
-                return NotFound(new ActionResponse { StatusCode = 404, Message = ActionResponseMessage.NotFound });
-            return Ok(new ActionResponse { Successful = true, StatusCode = 200, Message = ActionResponseMessage.Ok, Data = category });
-        }
-
-        [HttpPost("mobile/add/{name}")]
-        public async Task<ActionResult<ActionResponse>> AddMobileCategory(string name)
-        {
-            return Ok(new ActionResponse { Successful = true, StatusCode = 200, Message = ActionResponseMessage.Ok, Data = await _categoriesService.AddCategoryAsync(name) });
-        }
-
-        [HttpGet("mobile/get/{name}")]
-        public async Task<ActionResult<ActionResponse>> GetMobileCategories(string name)
         {
             return Ok(new ActionResponse { Successful = true, StatusCode = 200, Message = ActionResponseMessage.Ok, Data = await _categoriesService.GetCategoriesAsync(name) });
         }
