8f46bdfa23079654bbb3e2c8336137f718674d23
Ayo-mi <harabprince@gmail.com>
2022-10-27 00:08:45 +0100

    auth updated

--- ProTrendAPI/Models/User/ProfileDTO.cs
+++ ProTrendAPI/Models/User/ProfileDTO.cs
@@ -9,15 +9,9 @@ namespace ProTrendAPI.Models.User
         public string FullName { get; set; } = string.Empty;
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
-        public string AccountType { get; set; } = string.Empty;
-        public string BackgroundImageUrl { get; set; } = string.Empty;
-        public string ProfileImage { get; set; } = string.Empty;
-        public string PaymentPin { get; set; } = string.Empty;
-        public string AccountNumber { get; set; } = string.Empty;
-        public string Bank { get; set; } = string.Empty;
-        public string Country { get; set; } = string.Empty;
-        public string? Phone { get; set; } = null;
         [Required(ErrorMessage = "Password field can't be empty")]
         public string Password { get; set; } = string.Empty;
+        public string AccountType { get; set; } = string.Empty;
+        public string? Country { get; set; } = string.Empty;        
     }
 }
